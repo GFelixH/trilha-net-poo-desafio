49ab3e33435da7e290fb92054c7cd83514a332b4
Felix <gabritelos@hotmail.com>
2022-09-02 11:17:28 -0300

    Sistema Celulares

--- Program.cs
+++ Program.cs
@@ -4,11 +4,13 @@ class Program
     static void Main(string[] args)
     {
         // Novo Nokia
-        Nokia meuNokia = new Nokia(numero:"91234-5678",modelo: "5.3",iMEI: "16542345235", memoria: "128GB");
+        Nokia meuNokia = new Nokia(numero: "91234-5678", modelo: "5.3", iMEI: "16542345235", memoria: "128GB");
         meuNokia.MostrarInfo();
 
         // Novo iphone
         Iphone meuIphone = new Iphone(numero: "91234-5678", iMEI: "16542345235", memoria: "1TB", modelo: "13 PRO MAX");
         meuIphone.MostrarInfo();
+
     }
+
 }
